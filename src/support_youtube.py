044a545eda92a78a2f111642957ea5917100a7a4
JorgeLlorente <jorgellorente1994@gmail.com>
2023-01-21 18:34:05 +0100

    updating one_game_analysis and others

--- src/support_youtube.py
+++ src/support_youtube.py
@@ -5,10 +5,7 @@ import requests
 import re
 import sqlalchemy as alch
 from getpass import getpass
-import os
 from dotenv import load_dotenv
-import pickle
-import matplotlib.pyplot as plt
 
 
 # ------------------------------------------- YOUTUBE --------------------------------------------------
@@ -168,73 +165,3 @@ def youtube_tosql(game):
             ("{row['publishedat']}","{row['channeltitle']}","{row['duration']}","{row['viewcount']}","{row['likecount']}","{row['commentcount']}","{row['juego']}");""")
         except ValueError:
             print("este titulo no es valido en SQL")
-
-
-    # --------------------------------------- One game analysis ------------------------------------------
-
-def sql_connection():
-    db_name = "videogames_industry"
-    sql = os.getenv("MySQLPassword")
-    conexion = f"mysql+pymysql://root:{sql}@localhost/{db_name}"
-    engine = alch.create_engine(conexion)
-
-
-def select_sql_table(table):
-
-    # This function does the next three things
-    # 1. Function makes the connection with MySQL.
-    # 2. Function corrects columns type.
-    # 3. Function filter table by top 10 videogames.
-
-
-    db_name = "videogames_industry"
-    sql = os.getenv("MySQLPassword")
-    conexion = f"mysql+pymysql://root:{sql}@localhost/{db_name}"
-    engine = alch.create_engine(conexion)
-
-    query = f'''SELECT * FROM videogames_industry.{table};'''
-    df = pd.read_sql(query, engine)
-    if "fecha" in df.columns and "watch_time_hours" in df.columns:
-        df["fecha"] = pd.to_datetime(df["fecha"])
-        df["watch_time_hours"] = df["watch_time_hours"].astype("float")
-    elif "fecha" in df.columns:
-        df["fecha"] = pd.to_datetime(df["fecha"])
-
-    else:
-        pass
-    with open('../data/top10_games.pickle', 'rb') as game:
-        games = pickle.load(game)
-    df = df[df["videogame_id"].isin(games)]
-    return df
-
-
-def plotting(df_game, df_all, juego):
-
-    # In this function we will visualize timeline plots depending on the game we are choosing and
-    # the dataframes we have chosen. Dataframes depend on the columns we want to analyze.
-
-    plt.rcParams["figure.figsize"] = [8.0, 2.0]
-
-    for i, column in zip(range(len(list(df_game.columns))), list(df_game.columns)):
-        if column == "fecha":
-            pass
-        else:
-            fig = plt.figure(f"Figure {i}")
-            plt.title(f"mean of {column} in {juego}")
-            plt.xlabel("Year")
-            plt.ylabel(column)
-            # plt.legend()
-            plt.plot(df_all["fecha"],
-                    df_all[column],
-                    color = "grey",
-                    linewidth = 2,
-                    marker = "o")
-                    # label= "mean")
-
-            plt.plot(df_game["fecha"],
-                    df_game[column],
-                    color = "black",
-                    linewidth = 2,
-                    marker = "o")
-                    # label= "Fortnite")
-            plt.show()
