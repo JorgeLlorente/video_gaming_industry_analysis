e0c45995dd872c7e87b92f40730d4e8a33dfc05c
JorgeLlorente <jorgellorente1994@gmail.com>
2023-01-28 14:39:23 +0100

    streamlit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 import re
 import sqlalchemy as alch
 from getpass import getpass
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 import pickle
 import matplotlib.pyplot as plt
 import datetime
@@ -15,10 +15,3 @@ sys.path.append("../")
 
 import src.support as sp
 
-with open('data/top10_games.pickle', 'rb') as game:
-    games = pickle.load(game)
-list_games = games[1:]
-
-juego = st.selectbox('Pick one', list_games)
-
-st.write(sp.info(juego))
